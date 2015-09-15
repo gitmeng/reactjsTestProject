89b9560e548f7984dcc61e51c1b3629a4ce69983
Meng Su <mengsu@live.se>
2015-09-15 15:49:31 +0200

    First commit

---
+++ gulpfile.js
@@ -0,0 +1,35 @@
+var gulp = require('gulp');
+var concat = require('gulp-concat');
+var uglify = require('gulp-uglify');
+var react = require('gulp-react');
+var htmlreplace = require('gulp-html-replace');
+
+var path = {
+  HTML: 'src/index.html',
+  ALL: ['src/js/*.js', 'src/js/**/*.js', 'src/index.html'],
+  JS: ['src/js/*.js', 'src/js/**/*.js'],
+  MINIFIED_OUT: 'build.min.js',
+  DEST_SRC: 'dist/src',
+  DEST_BUILD: 'dist/build',
+  DEST: 'dist'
+};
+
+// Transform JSX into JS
+gulp.task('transform', function(){
+  gulp.src(path.JS)
+    .pipe(react())
+    .pipe(gulp.dest(path.DEST_SRC));
+});
+
+// Copies index.html to the dist folder
+gulp.task('copy', function(){
+  gulp.src(path.HTML)
+    .pipe(gulp.dest(path.DEST));
+});
+
+// Watch changes to all files and do transform and copy
+gulp.task('watch', function(){
+  gulp.watch(path.ALL, ['transform', 'copy']);
+});
+
+gulp.task('default', ['watch']);

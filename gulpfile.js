72eee0a1cd4449caacdc5adc952ea78050b3e200
Kristoffer Risendal <kristoffer.risendal@thomascook.se>
2015-09-15 16:28:57 +0200

    Added livereload to gulpfile

--- gulpfile.js
+++ gulpfile.js
@@ -3,33 +3,36 @@ var concat = require('gulp-concat');
 var uglify = require('gulp-uglify');
 var react = require('gulp-react');
 var htmlreplace = require('gulp-html-replace');
+var livereload = require('livereload');
 
 var path = {
-  HTML: 'src/index.html',
-  ALL: ['src/js/*.js', 'src/js/**/*.js', 'src/index.html'],
-  JS: ['src/js/*.js', 'src/js/**/*.js'],
-  MINIFIED_OUT: 'build.min.js',
-  DEST_SRC: 'dist/src',
-  DEST_BUILD: 'dist/build',
-  DEST: 'dist'
+    HTML: 'src/index.html',
+    ALL: ['src/js/*.js', 'src/js/**/*.js', 'src/index.html'],
+    JS: ['src/js/*.js', 'src/js/**/*.js'],
+    MINIFIED_OUT: 'build.min.js',
+    DEST_SRC: 'dist/src',
+    DEST_BUILD: 'dist/build',
+    DEST: 'dist'
 };
 
 // Transform JSX into JS
-gulp.task('transform', function(){
-  gulp.src(path.JS)
-    .pipe(react())
-    .pipe(gulp.dest(path.DEST_SRC));
+gulp.task('transform', function() {
+    gulp.src(path.JS)
+        .pipe(react())
+        .pipe(gulp.dest(path.DEST_SRC));
 });
 
 // Copies index.html to the dist folder
-gulp.task('copy', function(){
-  gulp.src(path.HTML)
-    .pipe(gulp.dest(path.DEST));
+gulp.task('copy', function() {
+    gulp.src(path.HTML)
+        .pipe(gulp.dest(path.DEST));
 });
 
 // Watch changes to all files and do transform and copy
-gulp.task('watch', function(){
-  gulp.watch(path.ALL, ['transform', 'copy']);
+gulp.task('watch', function() {
+    server = livereload.createServer();
+    server.watch(path.DEST_SRC);
+    gulp.watch(path.ALL, ['transform', 'copy']);
 });
 
 gulp.task('default', ['watch']);
